4c2ac6d26e47ee6dab4bf9744d54fa1e5334483f
AbdulRehmanAnsari35 <160354649+AbdulRehmanAnsari35@users.noreply.github.com>
2024-02-27 01:19:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Question1
+// console.log("Hello World");
+// Question2
+// var myname = 'Abdul Rehman Ansari';
+// var message = 'is a responsible person';
+// console.log(myname + " " + message);
---
+++ main.ts
@@ -0,0 +1,7 @@
+// Question1
+// console.log("Hello World");
+
+// Question2
+// let myname = 'Abdul Rehman Ansari';
+// let message = 'is a responsible person';
+// console.log(myname+" "+message);
